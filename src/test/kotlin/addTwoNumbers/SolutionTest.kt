08aac41d89f17b6e49ff1fcda453cb2b1a42670e
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-02-14 19:38:29 +0800

    refactor

--- src/test/kotlin/addTwoNumbers/SolutionTest.kt
+++ src/test/kotlin/addTwoNumbers/SolutionTest.kt
@@ -8,8 +8,8 @@ import kotlin.test.assertNull
 class SolutionTest {
     @Test
     fun `add two numbers test case 1`() {
-        val linkA = createLinkFromArray(intArrayOf(2, 4, 3))
-        val linkB = createLinkFromArray(intArrayOf(5, 6, 4))
+        val linkA = intArrayOf(2, 4, 3).buildLink()
+        val linkB = intArrayOf(5, 6, 4).buildLink()
         val result = Solution().addTwoNumbersRe(linkA, linkB)
         assertNotNull(result)
         assertEquals(result.`val`, 7)
@@ -19,8 +19,8 @@ class SolutionTest {
 
     @Test
     fun `add two numbers test case 2`() {
-        val linkA = createLinkFromArray(intArrayOf(0))
-        val linkB = createLinkFromArray(intArrayOf(0))
+        val linkA = intArrayOf(0).buildLink()
+        val linkB = intArrayOf(0).buildLink()
         val result = Solution().addTwoNumbersRe(linkA, linkB)
         assertNotNull(result)
         assertEquals(result.`val`, 0)
@@ -29,8 +29,8 @@ class SolutionTest {
 
     @Test
     fun `add two numbers test case 3`() {
-        val linkA = createLinkFromArray(intArrayOf(9, 9, 9, 9, 9, 9, 9))
-        val linkB = createLinkFromArray(intArrayOf(9, 9, 9, 9))
+        val linkA = intArrayOf(9, 9, 9, 9, 9, 9, 9).buildLink()
+        val linkB = intArrayOf(9, 9, 9, 9).buildLink()
         val result = Solution().addTwoNumbersRe(linkA, linkB)
         assertNotNull(result)
         assertEquals(result.`val`, 8)
@@ -46,23 +46,23 @@ class SolutionTest {
     @Test
     fun `create link function test`() {
         val array = intArrayOf(1, 2, 4)
-        val link = createLinkFromArray(array)
+        val link = array.buildLink()
         assertNotNull(link)
         assertEquals(link.`val`, 1)
         assertEquals(link.next?.`val`, 2)
         assertEquals(link.next?.next?.`val`, 4)
         assertEquals(link.next?.next?.next, null)
     }
+}
 
-    private fun createLinkFromArray(array: IntArray): ListNode? {
-        if (array.isEmpty()) return null
-        val head = ListNode(-1)
-        var current = head
-        array.forEach {
-            val node = ListNode(it)
-            current.next = node
-            current = node
-        }
-        return head.next
+private fun IntArray.buildLink(): ListNode? {
+    if (this.isEmpty()) return null
+    val head = ListNode(-1)
+    var current = head
+    this.forEach {
+        val node = ListNode(it)
+        current.next = node
+        current = node
     }
+    return head.next
 }
