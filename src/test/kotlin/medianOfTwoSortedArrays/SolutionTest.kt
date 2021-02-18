9d7d8acebae271a6e60cca795e62f9ed8c84e0af
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-02-18 22:05:31 +0800

    longest palindromic substring

--- src/test/kotlin/medianOfTwoSortedArrays/SolutionTest.kt
+++ src/test/kotlin/medianOfTwoSortedArrays/SolutionTest.kt
@@ -16,7 +16,7 @@ internal class SolutionTest {
     fun `test case 2`() {
         val nums1 = intArrayOf(1, 2)
         val nums2 = intArrayOf(3, 4)
-        val result = Solution().findMedianSortedArrays(nums1, nums2)
+        val result = Solution().findMedianSortedArraysV1(nums1, nums2)
         assertEquals(2.5, result)
     }
 
@@ -24,7 +24,7 @@ internal class SolutionTest {
     fun `test case 3`() {
         val nums1 = intArrayOf(0, 0)
         val nums2 = intArrayOf(0, 0)
-        val result = Solution().findMedianSortedArrays(nums1, nums2)
+        val result = Solution().findMedianSortedArraysV1(nums1, nums2)
         assertEquals(0.0, result)
     }
 
@@ -32,7 +32,7 @@ internal class SolutionTest {
     fun `test case 4`() {
         val nums1 = intArrayOf()
         val nums2 = intArrayOf(1)
-        val result = Solution().findMedianSortedArrays(nums1, nums2)
+        val result = Solution().findMedianSortedArraysV1(nums1, nums2)
         assertEquals(1.0, result)
     }
 
@@ -40,7 +40,7 @@ internal class SolutionTest {
     fun `test case 5`() {
         val nums1 = intArrayOf(2)
         val nums2 = intArrayOf()
-        val result = Solution().findMedianSortedArrays(nums1, nums2)
+        val result = Solution().findMedianSortedArraysV1(nums1, nums2)
         assertEquals(2.0, result)
     }
 }
