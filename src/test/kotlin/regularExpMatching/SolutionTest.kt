6457bec897f74c8a750df8db5594bf30b2df965f
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-02-19 11:52:00 +0800

    regular exp matching

---
+++ src/test/kotlin/regularExpMatching/SolutionTest.kt
@@ -0,0 +1,65 @@
+package regularExpMatching
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+internal class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun `test case 1`() {
+        val s = "aa"
+        val p = "a"
+        assertFalse(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 2`() {
+        val s = "aaa"
+        val p = "a*a"
+        assertTrue(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 3`() {
+        val s = "ab"
+        val p = ".*"
+        assertTrue(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 4`() {
+        val s = "aab"
+        val p = "c*a*b"
+        assertTrue(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 5`() {
+        val s = "mississppi"
+        val p = "mis*is*p*"
+        assertFalse(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 6`() {
+        val s = "ab"
+        val p = ".*c"
+        assertFalse(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 7`() {
+        val s = "aaaa"
+        val p = "aaaaa"
+        assertFalse(solution.isMatch(s, p))
+    }
+
+    @Test
+    fun `test case 8`() {
+        val s = "aaa"
+        val p = "ab*a*c*a"
+        assertTrue(solution.isMatch(s, p))
+    }
+}
