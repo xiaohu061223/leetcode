8917351a72ccb4ee0472828b5c559be3646434d4
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-02-20 22:08:51 +0800

    fix comment

--- src/main/kotlin/threeSum/Solution.kt
+++ src/main/kotlin/threeSum/Solution.kt
@@ -1,5 +1,8 @@
 package threeSum
 
+/**
+ * 给你一个包含 n 个整数的数组nums，判断nums中是否存在三个元素 a，b，c ，使得a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ */
 class Solution {
     fun threeSum(nums: IntArray): List<List<Int>> {
         if (nums.size < 3) return emptyList()
