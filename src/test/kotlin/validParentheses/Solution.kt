5bff4dc8396fc73484725456f032711db5c40096
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-02-22 17:24:14 +0800

    valid parentheses refactor

--- src/test/kotlin/validParentheses/Solution.kt
+++ src/test/kotlin/validParentheses/Solution.kt
@@ -1,21 +1,22 @@
 package validParentheses
 
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ */
 class Solution {
     fun isValid(s: String): Boolean {
         if (s.isEmpty()) return false
         var stack = charArrayOf()
+        val parenthesesMap = mapOf('{' to '}', '[' to ']', '(' to ')')
         for (i in s.indices) {
-            when {
-                s[i] == '{' || s[i] == '[' || s[i] == '(' -> {
-                    stack = stack.plus(s[i])
-                }
-                else -> {
-                    if (stack.isEmpty()) return false
-                    if ((s[i] == '}' && stack.last() == '{') ||
-                        (s[i] == ']' && stack.last() == '[') ||
-                        (s[i] == ')' && stack.last() == '(')
-                    ) stack = stack.dropLast(1).toCharArray() else return false
-                }
+            stack = if (parenthesesMap.containsKey(s[i])) {
+                stack.plus(s[i])
+            } else {
+                if (parenthesesMap[stack.last()]!! == s[i]) stack.dropLast(1).toCharArray()
+                else return false
             }
         }
         return stack.isEmpty()
