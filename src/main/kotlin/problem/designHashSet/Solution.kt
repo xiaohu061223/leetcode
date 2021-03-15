fb0f259a187b33d41e0c55477273f064b15dd7f7
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-03-15 09:23:40 +0800

    design hashSet

--- src/main/kotlin/problem/designHashSet/Solution.kt
+++ src/main/kotlin/problem/designHashSet/Solution.kt
@@ -1,15 +1,59 @@
 package problem.designHashSet
 
 class Solution {
+    private val array = MutableList<Node?>(131072) { null }
     fun add(key: Int) {
-
+        val pos = hash(key)
+        if (array[pos] == null) {
+            array[pos] = Node(key)
+        } else {
+            var current = array[pos]
+            if (current?.value == key) return
+            while (current?.next != null) {
+                current = current.next
+                if (current?.value == key) return
+            }
+            current?.next = Node(key)
+        }
     }
 
     fun remove(key: Int) {
-
+        val pos = hash(key)
+        if (array[pos] == null) return
+        else {
+            var current = array[pos]
+            if (current?.value == key) {
+                array[pos] = current.next
+                return
+            }
+            var before = array[pos]
+            while (current != null) {
+                if (current.value == key) {
+                    before?.next = current.next
+                    return
+                }
+            }
+        }
     }
 
     fun contains(key: Int): Boolean {
+        val pos = hash(key)
+        if (array[pos] == null) {
+            return false
+        } else {
+            var current = array[pos]
+            while (current != null) {
+                if (current.value == key) return true
+                current = current.next
+            }
+        }
         return false
     }
+
+    private fun hash(key: Int) = key % (131072)
+
+    class Node(
+        val value: Int,
+        var next: Node? = null,
+    )
 }
--- src/test/kotlin/problem/designHashSet/SolutionTest.kt
+++ src/test/kotlin/problem/designHashSet/SolutionTest.kt
@@ -1,7 +1,22 @@
 package problem.designHashSet
 
-import core.ISolutionTest
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-internal class SolutionTest : ISolutionTest {
+internal class SolutionTest {
+    private val solution = Solution()
+
+    @Test
+    fun `test case`() {
+        val hashSet = solution
+        hashSet.add(1)
+        hashSet.add(2)
+        assertThat(hashSet.contains(1)).isTrue
+        assertThat(hashSet.contains(3)).isFalse
+        hashSet.add(2)
+        assertThat(hashSet.contains(2)).isTrue
+        hashSet.remove(2)
+        assertThat(hashSet.contains(2)).isFalse
+    }
 
 }
