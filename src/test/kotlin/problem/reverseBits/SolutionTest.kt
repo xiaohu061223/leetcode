247d8c3e33dde22f62a851470de8cbcc4d769a7a
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-03-29 13:06:53 +0800

    refactor

--- src/test/kotlin/problem/reverseBits/SolutionTest.kt
+++ src/test/kotlin/problem/reverseBits/SolutionTest.kt
@@ -6,11 +6,9 @@ import core.ISolutionTest
 internal class SolutionTest : ISolutionTest<Int, Int>() {
     private val solution = Solution()
 
-    override val solutions: List<ISolution<Int, Int>>
-        get() = listOf(solution)
-    override val testCases: List<Pair<Int, Int>>
-        get() = listOf(
-            Pair(43261596, 964176192),
-            Pair(-3, -1073741825),
-        )
+    override val solutions: List<ISolution<Int, Int>> = listOf(solution)
+    override val testCases: List<Pair<Int, Int>> = listOf(
+        Pair(43261596, 964176192),
+        Pair(-3, -1073741825),
+    )
 }
