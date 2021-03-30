0743773bccd746f42f2d52c0956a39c9efe34161
hcy1223 <chenyuan.hu@thoughtworks.com>
2021-03-30 13:54:35 +0800

    format code

--- src/test/kotlin/problem/mergeKSortedLists/SolutionTest.kt
+++ src/test/kotlin/problem/mergeKSortedLists/SolutionTest.kt
@@ -7,10 +7,15 @@ import core.ISolution
 import core.ISolutionTest
 
 internal class SolutionTest : ISolutionTest<Array<ListNode?>, ListNode?>() {
+
     override val solutions: List<ISolution<Array<ListNode?>, ListNode?>> = listOf(Solution())
+
     override val testCases: List<Pair<Array<ListNode?>, ListNode?>> = listOf(
-        arrayOf(singlyLinkedListOf(1, 4, 5), singlyLinkedListOf(1, 3, 4), singlyLinkedListOf(2, 6))
-                to singlyLinkedListOf(1, 1, 2, 3, 4, 4, 5, 6),
+        arrayOf(
+            singlyLinkedListOf(1, 4, 5),
+            singlyLinkedListOf(1, 3, 4),
+            singlyLinkedListOf(2, 6),
+        ) to singlyLinkedListOf(1, 1, 2, 3, 4, 4, 5, 6),
         emptyArray<ListNode?>() to emptyLinkedList(),
         arrayOf(emptyLinkedList()) to emptyLinkedList(),
     )
