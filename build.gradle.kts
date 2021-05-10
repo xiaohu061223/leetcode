bf0e45f4d6eca607d30a1e81e78cac76ec4dd779
luigi2077 <chenyuan_hu@outlook.com>
2021-05-10 14:04:34 +0800

    tech: upgrade kotlin to 1.5.0-release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30"
+    kotlin("jvm") version "1.5.0"
     id("org.jetbrains.dokka") version "1.4.20"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
 }
