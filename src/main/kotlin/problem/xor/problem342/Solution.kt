196ed6a5dd1e581e570d7b02ddef15f7c1dd9390
luigi2077 <chenyuan_hu@outlook.com>
2021-05-31 17:35:57 +0800

    refactor: problem 342 is power of four

--- src/main/kotlin/problem/xor/problem342/Solution.kt
+++ src/main/kotlin/problem/xor/problem342/Solution.kt
@@ -4,10 +4,8 @@ import core.ISolution
 
 class Solution : ISolution<Int, Boolean> {
     private fun isPowerOfFour(n: Int): Boolean {
-        if (n == 1) return true
-        if (n < 1) return false
         val binaryStr = Integer.toBinaryString(n)
-        return binaryStr[0] == '1' && (n.and(n - 1)) == 0 && (binaryStr.length - 1) % 2 == 0
+        return n >= 1 && binaryStr[0] == '1' && (n.and(n - 1)) == 0 && (binaryStr.length - 1) % 2 == 0
     }
 
     override fun run(input: Int): Boolean = isPowerOfFour(input)
